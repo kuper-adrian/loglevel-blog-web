9eeb004f5d3b5e194a6e014791831a333ba680a8
Adrian Kuper <adrian.kuper@hotmail.de>
2018-09-23 16:47:42 +0200

    The index page now accepts get parameters for displaying the snackbar.
    This can be used to redirect to the index page on error or success when
    the original page isn't displayable or not needed anymore.

--- app/routes/index.js
+++ app/routes/index.js
@@ -21,7 +21,20 @@ router
         next(error);
       });
   }, (req, res) => {
-    res.render('index', { title: 'loglevel: blog', data: req.devNullPosts });
+    // if get parameters for the snackbar were passed
+    if (req.query.sbType && req.query.sbText) {
+      // render page with snackbar
+      res.render(
+        'index',
+        {
+          title: 'loglevel: blog',
+          data: req.devNullPosts,
+          snackbarMessage: { type: req.query.sbType, text: req.query.sbText },
+        },
+      );
+    } else {
+      res.render('index', { title: 'loglevel: blog', data: req.devNullPosts });
+    }
   });
 
 module.exports = router;
