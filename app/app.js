2036a4a2afe1fbe7c27ae12463ca48ed1171372a
Adrian Kuper <adrian.kuper@hotmail.de>
2018-11-14 19:55:30 +0100

    fixed error page

--- app/app.js
+++ app/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
@@ -33,12 +34,12 @@ app.use('/licenses', licensesRouter);
 app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
-app.use((req, res, next) => {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use((err, req, res) => {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
